0710e5cb44d2cff186aa620103b2c4a9e2d4161f
wangzhengkai <zjutwzk@163.com>
2021-05-26 22:56:50 +0800

    release

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/lib',
+  base: '/wjl-choujiang/lib',
   build: {
     outDir: 'lib'
   }
